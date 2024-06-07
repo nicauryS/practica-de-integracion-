3d62eddda0f0784c5f3909ba1686c4deb30e150d
nicauryS <119086333+nicauryS@users.noreply.github.com>
2024-06-07 14:28:18 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    // Método principal
+    public static void main(String[] args) {
+        // Código dentro del método principal
+        System.out.println("Hola Mundo");
+        
+        // Otro bloque de código, por ejemplo, un bucle
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Número: " + i);
+        } // Fin del bucle
+        
+    } // Fin del método principal
+} // Fin de la clase Main
+
---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+function holaMundo() {
+    return "Hola Mundo";
+}
+
+module.exports = holaMundo;
+
+console.log(holaMundo());
